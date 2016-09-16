6d15fc6f89f571b81d0165713725e38e5c49317a
Dan Riley <dxriley@gmail.com>
2016-09-16 17:01:14 -0600

    Final code checkin

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { NavController } from 'ionic-angular';
 
 import { WeatherService } from "../../providers/weather-service/weather-service";
 
-interface City {
-	name: string;
-	apiCity: string;
-}
+//TODO: figure how to do interfaces and initialize fctn... or perhaps do objects --Riley
+// interface City {
+//   name: string;
+//   apiCity: string;
+// }
 
 // interface FullDayForcast {
 //         dayOfWeek: string;
@@ -45,79 +46,87 @@ export class HomePage {
   		.then(weatherData => {
         //Assumption data will stay cronologically ordered and ascending
         this.fiveDayEvery3Hours = weatherData["list"];
-	  		console.log("fiveDayEvery3Hours-->" + JSON.stringify(this.fiveDayEvery3Hours));
-          var aDate = new Date();          
-          var currentDate = aDate.getMonth()+1+"/"+aDate.getDate()+"/"+aDate.getFullYear();
-          var currentTemp = 0;
-          var currentHumidity = 0;
-          var i = 0;
-          console.log("1");
-          this.basicFiveDayForcast = [];
-          var oneDayForcast = {
-                     dayOfWeek: null,
-                          date: null,
-                       lowTemp: 1000,
-                      highTemp: -1000,
-                   currentTemp: 1000,
-               currentHumidity: 1000,
-                   weatherMain: null,
-                   weatherIcon: null,
-              threeHourForcast:  []
+	  		//console.log("fiveDayEvery3Hours-->" + JSON.stringify(this.fiveDayEvery3Hours)); 
+        var previousDate = null;
+        var theDate = null;
+        var currentTemp = 0;
+        var currentHumidity = 0;
+        var i = 0;
+
+        this.basicFiveDayForcast = [];
+
+        var oneDayForcast = {
+                   dayOfWeek: null,
+                        date: null,
+                     lowTemp: 1000,
+                    highTemp: -1000,
+                 currentTemp: 1000,
+             currentHumidity: 1000,
+                 weatherMain: null,
+                 weatherIcon: null,
+            threeHourForcast:  []
+          };
+        var tempDay3HourForcastArray = [];
+        for (var j = 0; j <= this.fiveDayEvery3Hours.length - 1; j++) {
+          // var aDate = new Date(this.fiveDayEvery3Hours[j].dt*1000); //REMOVE: Miss understood this value --Riley
+          //2016-09-17 09:00:00 //REMOVE: dt_txt format for substring --Riley
+
+          var theDay = this.fiveDayEvery3Hours[j].dt_txt.substring(8,10);
+          var theMonth = this.fiveDayEvery3Hours[j].dt_txt.substring(5,7);
+          var theYear = this.fiveDayEvery3Hours[j].dt_txt.substring(0,4);
+          theDate = theMonth+"/"+theDay+"/"+theYear;
+          if (j==0) {
+            previousDate = theDate;
+          }
+          //Store and prep for new day
+          if (theDate != previousDate) { 
+            //TODO: dup. code refactor to function --Riley
+            oneDayForcast.date = previousDate;
+            oneDayForcast.dayOfWeek = this.dayOfTheWeek[(new Date(previousDate)).getDay()];
+            oneDayForcast.threeHourForcast = tempDay3HourForcastArray;
+            tempDay3HourForcastArray = [];
+            this.basicFiveDayForcast.push(oneDayForcast);
+
+            oneDayForcast = { 
+                       dayOfWeek: null,
+                            date: null,
+                         lowTemp: 1000,
+                        highTemp: -1000,
+                     currentTemp: 1000,
+                 currentHumidity: 1000,
+                     weatherMain: null,
+                     weatherIcon: null,
+                threeHourForcast:[]
             };
-          // console.log("2");
-          // oneDayForcast.lowTemp = 1000;
-          // console.log("3");
-          // oneDayForcast.highTemp = -1000;
-          var tempDay3HourForcastArray = [];
-          for (var j = 0; j <= this.fiveDayEvery3Hours.length - 1; j++) {
-            aDate = new Date(this.fiveDayEvery3Hours[j].dt*1000);
-            var theDay = aDate.getDate();
-            var theMonth = aDate.getMonth()+1;
-            var theYear = aDate.getFullYear();
-            var theDate = theMonth+"/"+theDay+"/"+theYear;
-            console.log("*******this.fiveDayEvery3Hours["+j+"].temp->"+ this.fiveDayEvery3Hours[j].main.temp);
-//             previousHumitity = this.f
+            // i = i + 1;
+            previousDate = theDate;
+          }
+          // console.log("*******this.fiveDayEvery3Hours["+j+"].temp->"+ this.fiveDayEvery3Hours[j].main.temp
+          //   +" ---- "+this.fiveDayEvery3Hours[j].dt_txt);
+          // console.log("previousDate->"+previousDate+ " : theDate->"+theDate)
+          //TODO: get other weather stuff --Riley
+          tempDay3HourForcastArray.push(this.fiveDayEvery3Hours[j]);
+          if (oneDayForcast.highTemp < this.fiveDayEvery3Hours[j].main.temp) {
+            oneDayForcast.highTemp = this.fiveDayEvery3Hours[j].main.temp;
+          }
+          if (oneDayForcast.lowTemp > this.fiveDayEvery3Hours[j].main.temp) {
+            oneDayForcast.lowTemp = this.fiveDayEvery3Hours[j].main.temp;
+          }
+          //TODO: Add: just take the mainweather as day's word forcast ie(Sunny) --Riley
+          if (true == true) { //Perhaps pick noon as the weater word of the day. --Riley
+            // NOTE: weather icons: http://openweathermap.org/img/w/01d.png --Riley
 
-// dasfasasfasfasd
+          }
+        }//end for loop
+        //Store last day
+        //TODO: dup. code refactor to function --Riley
+        oneDayForcast.date = previousDate;
+        oneDayForcast.dayOfWeek = this.dayOfTheWeek[(new Date(previousDate)).getDay()];
+        oneDayForcast.threeHourForcast = tempDay3HourForcastArray;
+        tempDay3HourForcastArray = [];
+        this.basicFiveDayForcast.push(oneDayForcast);
 
-            tempDay3HourForcastArray.push(this.fiveDayEvery3Hours[j]);
-
-            if (oneDayForcast.highTemp < this.fiveDayEvery3Hours[j].main.temp) {
-              oneDayForcast.highTemp = this.fiveDayEvery3Hours[j].main.temp;
-            }
-            if (oneDayForcast.lowTemp > this.fiveDayEvery3Hours[j].main.temp) {
-              oneDayForcast.lowTemp = this.fiveDayEvery3Hours[j].main.temp;
-            }
-            //just take the mainweather as day's word forcast ie(Sunny)
-            if (true == true){
-
-            }
-            console.log("*******Date->"+ theDate);
-            if (theDate != currentDate || j == this.fiveDayEvery3Hours.length - 1) {//|| j == this.fiveDayEvery3Hours.length - 1
-              console.log("**------ day"+i+1);
-              oneDayForcast.date = currentDate;
-              oneDayForcast.dayOfWeek = this.dayOfTheWeek[(new Date(currentDate)).getDay()];
-              oneDayForcast.threeHourForcast = tempDay3HourForcastArray;
-              tempDay3HourForcastArray = [];
-              this.basicFiveDayForcast.push(oneDayForcast);
-              // oneDayForcast = new FullDayForcast; 
-              oneDayForcast = { 
-                         dayOfWeek: null,
-                              date: null,
-                           lowTemp: 1000,
-                          highTemp: -1000,
-                       currentTemp: 1000,
-                   currentHumidity: 1000,
-                       weatherMain: null,
-                       weatherIcon: null,
-                  threeHourForcast:[]
-              };
-              i = i + 1;
-              currentDate = theDate;
-            }
-            // this.basicFiveDayForcast[i] = this.fiveDayEvery3Hours[j]
-          }//end for
-        console.log("this.basicFive-->"+ JSON.stringify(this.basicFiveDayForcast));
+        //console.log("this.basicFive-->"+ JSON.stringify(this.basicFiveDayForcast));
 	  		this.isDoneLoading = true;
   		})
   		.catch(err => {
@@ -129,4 +138,3 @@ export class HomePage {
 }
 
 
-//weather icons: http://openweathermap.org/img/w/01d.png
